475944806317d3af2cf2331169ca7650e9413793
emanud <emanud@live.it>
2019-09-23 12:58:54 +0200

    Update readData.py

--- readData.py
+++ readData.py
@@ -2,7 +2,8 @@ import sqlite3
 import pandas as pd
 
 conn=sqlite3.connect('Data/database.sqlite')
-Match=pd.read_sql("SELECT * FROM Match;",conn)
+Match_Train=pd.read_sql("SELECT * FROM Match WHERE season!='2015/2016';",conn)
+Match_Test=pd.read_sql("SELECT * FROM Match WHERE season='2015/2016';",conn)
 Country=pd.read_sql("SELECT * FROM Country;",conn)
 League=pd.read_sql("SELECT * FROM League;",conn)
 
