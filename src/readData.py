32630d4efcec9b8c35149d8cb4e9da0acf70c0ef
Samir Si-Mohammed <samirsim98@gmail.com>
2019-09-25 10:08:55 +0200

    Changing variable names upper case

--- src/readData.py
+++ src/readData.py
@@ -2,13 +2,13 @@ import sqlite3
 import pandas as pd
 
 conn = sqlite3.connect('Data/database.sqlite')
-Match_Train = pd.read_sql("SELECT * FROM Match WHERE season!='2015/2016';", conn)
-Match_Test = pd.read_sql("SELECT * FROM Match WHERE season='2015/2016';", conn)
-Country = pd.read_sql("SELECT * FROM Country;", conn)
-League = pd.read_sql("SELECT * FROM League;", conn)
+matchTrain = pd.read_sql("SELECT * FROM Match WHERE season!='2015/2016';", conn)
+matchTest = pd.read_sql("SELECT * FROM Match WHERE season='2015/2016';", conn)
+country = pd.read_sql("SELECT * FROM Country;", conn)
+league = pd.read_sql("SELECT * FROM League;", conn)
 
-Team = pd.read_sql("SELECT * FROM Team;", conn)
-Team_Attributes = pd.read_sql("SELECT * FROM Team_Attributes;", conn)
-Player = pd.read_sql("SELECT * FROM Player;", conn)
-Player_Attributes = pd.read_sql("SELECT * FROM Player_Attributes;", conn)
+team = pd.read_sql("SELECT * FROM Team;", conn)
+teamAttributes = pd.read_sql("SELECT * FROM Team_Attributes;", conn)
+player = pd.read_sql("SELECT * FROM Player;", conn)
+playerAttributes = pd.read_sql("SELECT * FROM Player_Attributes;", conn)
 
